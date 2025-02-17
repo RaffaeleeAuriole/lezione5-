fb7b1c39d308771f3dfa6f63af505c355caf7f05
RaffaeleeAuriole <164177637+RaffaeleeAuriole@users.noreply.github.com>
2025-02-17 16:07:50 +0100

    ingrandimento

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
-let centerX = 300;
-let centerY = 300;
+let centerX = 450;  
+let centerY = 450;  
 let totalPoints = 0;
 let hits = 0;
 
 function setup() {
-    let cnv = createCanvas(600, 600);
+    let cnv = createCanvas(900, 900);  
     cnv.parent('canvas');
     cnv.mousePressed(checkHit);
     noLoop();
@@ -12,11 +12,11 @@ function setup() {
 
 function draw() {
     clear();
-    drawTarget(width / 2, height / 2, 400);
+    drawTarget(width / 2, height / 2, 600);  
 }
 
 function drawTarget(x, y, size) {
-    let colors = ['white', 'black', 'blue', 'red', 'yellow'];
+    let colors = ['white', 'grey', 'blue', 'red', 'yellow'];
     let rings = colors.length;
     let step = size / rings;
     for (let i = 0; i < rings; i++) {
@@ -34,20 +34,20 @@ function checkHit() {
     let score = 0;
     let feedback = "";
 
-    if (d < 40) {
+    if (d < 60) {  
         score = 35;
         feedback = "Bravo! Hai fatto centro, +35pt";
         hits++; 
-    } else if (d < 80) {
+    } else if (d < 120) {  
         score = 25;
         feedback = "Ci sei quasi, +25pt";
-    } else if (d < 120) {
+    } else if (d < 180) {  
         score = 15;
         feedback = "Non mollare, +15pt";
-    } else if (d < 160) {
+    } else if (d < 240) {  
         score = 10;
         feedback = "Devi allenarti di più, +10pt";
-    } else if (d < 200) {
+    } else if (d < 300) {  
         score = 5;
         feedback = "Non ci siamo, +5pt";
     }
@@ -63,7 +63,7 @@ function checkHit() {
     pointsDisplay.textContent = "Punti Totali: " + totalPoints;
 
     stroke(0);
-    strokeWeight(5);
-    line(mouseX - 5, mouseY, mouseX + 5, mouseY); // Linea orizzontale
-    line(mouseX, mouseY - 5, mouseX, mouseY + 5); // Linea verticale
+    strokeWeight(7);  
+    line(mouseX - 7, mouseY, mouseX + 7, mouseY);  
+    line(mouseX, mouseY - 7, mouseX, mouseY + 7);  
 }
