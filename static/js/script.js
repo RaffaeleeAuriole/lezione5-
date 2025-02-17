57c489551b6ef50dfe8c888eb63da93e8f3ee626
RaffaeleeAuriole <164177637+RaffaeleeAuriole@users.noreply.github.com>
2025-02-17 16:16:17 +0100

    .

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
-let centerX = 450;  
-let centerY = 450;  
+let centerX = 450;
+let centerY = 450;
 let totalPoints = 0;
 let hits = 0;
 
 function setup() {
-    let cnv = createCanvas(900, 900);  
+    let cnv = createCanvas(900, 900);
     cnv.parent('canvas');
     cnv.mousePressed(checkHit);
     noLoop();
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
     clear();
-    drawTarget(width / 2, height / 2, 600);  
+    drawTarget(width / 2, height / 2, 600);
 }
 
 function drawTarget(x, y, size) {
@@ -34,20 +34,20 @@ function checkHit() {
     let score = 0;
     let feedback = "";
 
-    if (d < 60) {  
+    if (d < 60) {
         score = 35;
         feedback = "Bravo! Hai fatto centro, +35pt";
         hits++; 
-    } else if (d < 120) {  
+    } else if (d < 120) {
         score = 25;
         feedback = "Ci sei quasi, +25pt";
-    } else if (d < 180) {  
+    } else if (d < 180) {
         score = 15;
         feedback = "Non mollare, +15pt";
-    } else if (d < 240) {  
+    } else if (d < 240) {
         score = 10;
         feedback = "Devi allenarti di più, +10pt";
-    } else if (d < 300) {  
+    } else if (d < 300) {
         score = 5;
         feedback = "Non ci siamo, +5pt";
     }
@@ -62,8 +62,10 @@ function checkHit() {
     counter.textContent = "Centri: " + hits;
     pointsDisplay.textContent = "Punti Totali: " + totalPoints;
 
-    stroke(0);
-    strokeWeight(7);  
-    line(mouseX - 7, mouseY, mouseX + 7, mouseY);  
-    line(mouseX, mouseY - 7, mouseX, mouseY + 7);  
+    if (d < 300) { // Se il clic è dentro il cerchio
+        stroke(0);
+        strokeWeight(7);
+        line(mouseX - 7, mouseY, mouseX + 7, mouseY); // Linea orizzontale
+        line(mouseX, mouseY - 7, mouseX, mouseY + 7); // Linea verticale
+    }
 }
